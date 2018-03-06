ff505be8371c3ee3a61c51e1f78fe1a319f47068
docteurdux <xavierdpt+docteurdux@gmail.com>
2018-03-06 23:54:38 +0100

    docker cleanup

--- build.sh
+++ build.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-# for hash in `docker ps -a | awk '{print $1,$2}' | grep 'hadoop$' | cut -f 1 -d ' '` ; do
-	# docker container rm $hash
-# done
-
-# for i in `docker images --filter="dangling=true" -q`; do
-	# for hash in `docker ps -a | awk '{print $1,$2}' | grep "$i"'$' | cut -f 1 -d ' '` ; do
-		# docker container rm $hash
-	# done
-	# docker image rm $i
-# done
-
-
-# docker image inspect hadoop >/dev/null && docker image rm hadoop
 docker build -t hadoop .
 
